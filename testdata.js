5dd50de19914bd81dc49307673e61b4b4fb206da
Jake Hartz <jhartz@outlook.com>
2014-11-23 01:06:44 -0500

    Added opacities for layers

--- testdata.js
+++ testdata.js
@@ -37,6 +37,7 @@ var SERGIS_JSON_DATA = {
                             "layers": [
                                 {
                                     "name": "Overcrowding",
+                                    "opacity": 1,
                                     "urls": [
                                         "http://geoapps64.main.ad.rit.edu:6080/arcgis/rest/services/malmo/Malmo_test/MapServer"
                                         // MALM_Trygghet_levnadsf_tr(a with a circle on top)ngbodd_12_SWE
@@ -44,6 +45,7 @@ var SERGIS_JSON_DATA = {
                                 },
                                 {
                                     "name": "Illness",
+                                    "opacity": 0.85,
                                     "urls": [
                                         "http://geoapps64.main.ad.rit.edu:6080/arcgis/rest/services/malmo/Malmo_test/MapServer"
                                         // MALM_Trygghet_levnadsf_oh(a with 2 dots on top)lsa_02_11_SWE
@@ -51,6 +53,7 @@ var SERGIS_JSON_DATA = {
                                 },
                                 {
                                     "name": "Injury",
+                                    "opacity": 0.5,
                                     "urls": [
                                         "http://geoapps64.main.ad.rit.edu:6080/arcgis/rest/services/malmo/Malmo_test/MapServer"
                                         // MALM_Trygghet_levnadsf_kr(a with 2 dots on top)nkning_12_SWE
